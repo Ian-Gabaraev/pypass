0e0a9c0101814ac4663f2978174e1cff8e1a56a7
Ian Gabaraev <39344857+Ian-Gabaraev@users.noreply.github.com>
2019-11-13 00:09:19 +0300

    Update pypass.py

--- pypass.py
+++ pypass.py
@@ -4,7 +4,8 @@ import requests
 alphabet_uppercase = [chr(x) for x in range(65, 90)]
 alphabet_lowercase = [chr(x) for x in range(97, 122)]
 digits = [str(x) for x in range(10)]
-special_symbols = [chr(x) for x in range(33, 47)] + [chr(x) for x in range(58, 64)] + [chr(x) for x in range(91, 96)] + [chr(x) for x in range(123, 126)]
+special_symbols = [chr(x) for x in range(33, 47)] + [chr(x) for x in range(
+    58, 64)] + [chr(x) for x in range(91, 96)] + [chr(x) for x in range(123, 126)]
 
 password_complexity = {
     'all_upper': alphabet_uppercase,
@@ -16,12 +17,16 @@ password_complexity = {
     'mixed': alphabet_lowercase + alphabet_uppercase + digits + special_symbols
 }
 
+# Check remote dictionary whether password is a real word
+
+
 def not_a_word(password):
     if password.isalpha():
         r = requests.get('https://www.infoplease.com/dictionary/' + password)
         return r.status_code == 404
     else:
         return True
+
 
 def generate_password(length, complexity):
     password = ""
@@ -30,10 +35,5 @@ def generate_password(length, complexity):
     if not_a_word(password):
         return password
 
+# Test case
 print generate_password(12, password_complexity['all_upper'])
-print generate_password(12, password_complexity['all_lower'])
-print generate_password(12, password_complexity['alphabet'])
-print generate_password(12, password_complexity['all_upper_with_digits'])
-print generate_password(12, password_complexity['all_lower_with_digits'])
-print generate_password(12, password_complexity['digits_and_alphabet_ci'])
-print generate_password(12, password_complexity['mixed'])
